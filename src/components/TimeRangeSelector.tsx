861bbec9c13017f65e3c757b71bbc9e30f3cca23
Yan <yangabriel2012@gmail.com>
2023-05-29 22:45:26 -0300

    feat: criando component TimeRangeSelector

---
+++ src/components/TimeRangeSelector.tsx
@@ -0,0 +1,34 @@
+export type TimeRange = "long_term" | "short_term" | "medium_term"
+
+export interface TimeRangeSelectorProps{
+    typeTime: string,
+    getTopTracksOfTimeRange: (newTimeRange: TimeRange) => void
+}
+export const TimeRangeSelector = ({getTopTracksOfTimeRange, typeTime}: TimeRangeSelectorProps) =>{
+    return(
+        <div className="w-full flex justify-center pt-8 mb-12">
+              <span
+                className={`px-4 ${typeTime === "long_term" && "selectedTime"}`}
+                onClick={() => getTopTracksOfTimeRange("long_term")}
+              >
+                All time
+              </span>
+              <span
+                className={`px-4 ${
+                  typeTime === "short_term" && "selectedTime"
+                }`}
+                onClick={() => getTopTracksOfTimeRange("short_term")}
+              >
+                Weekend
+              </span>
+              <span
+                className={`px-4 ${
+                  typeTime === "medium_term" && "selectedTime"
+                }`}
+                onClick={() => getTopTracksOfTimeRange("medium_term")}
+              >
+                Mouth
+              </span>
+            </div>
+    )
+}
