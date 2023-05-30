0ed5beb840cd16a50e9ee262e5b625a199fb0ca6
Yan <yangabriel2012@gmail.com>
2023-05-29 22:44:34 -0300

    feat: criando component HeaderTracks

---
+++ src/components/HeaderTracks.tsx
@@ -0,0 +1,10 @@
+interface HeaderTracksProps{
+    text: string
+}
+export const HeaderTracks = ({text}: HeaderTracksProps) => {
+  return (
+    <div className="w-full flex justify-center text-2xl">
+      <h3 className="mb-4"> My Top {text} Tracks</h3>
+    </div>
+  );
+};
