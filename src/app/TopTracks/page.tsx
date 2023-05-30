41c343cb1b2974710895744182f8e78caea61b3f
Yan <yangabriel2012@gmail.com>
2023-05-29 22:43:19 -0300

    adicionando tela de tracks

--- src/app/TopTracks/page.tsx
+++ src/app/TopTracks/page.tsx
@@ -1,5 +1,111 @@
-export default function TopTracks(){
-    return(
-        <h1>TopTracks</h1>
-    )
+'use client'
+import { TopFiveTracks } from "@/components/TopFiveTracks";
+import { Loader } from "@/icons/Loader";
+import { useState, useEffect, useRef } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
+import SpotifyIcon from "@/icons/SpotifyIcon";
+import { LimitSelector } from "@/components/LimitSelector";
+import { TimeRangeSelector, TimeRange } from "@/components/TimeRangeSelector";
+import { HeaderTracks } from "@/components/HeaderTracks";
+import { PrintContainer } from "@/components/PrintContainer";
+import {useSearchParams} from 'next/navigation'
+
+function TopTracks() {
+  const domEl = useRef<HTMLDivElement>(null);
+  const [tracks, setTracks] = useState<SpotifyApi.TrackObjectFull[]>([]);
+  const [isLoading, setIsloading] = useState<boolean>(true);
+  const [typeTime, setTypeTime] = useState<TimeRange>("long_term");
+  const [limitTracks, setLimitTracks] = useState<number>(5)
+  const paramslog = useSearchParams()
+
+  const spotifyApi = new SpotifyWebApi();
+
+
+  const showTypeTime = (typeTime: TimeRange): string => {
+    switch(typeTime) {
+      case "long_term":
+        return "All Time";
+      case "short_term":
+        return "Weekend";
+      case "medium_term":
+        return "Mouth";
+      default:
+        return "";
+    }
+  };
+
+  //Chama a request tracks
+  const getTopTracks = (
+    timeRange: TimeRange, limit: number
+  ) => {
+    setTypeTime;
+    spotifyApi
+      .getMyTopTracks({ time_range: timeRange, limit: limit })
+      .then((response) => {
+        setTracks(response.items);
+        setIsloading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  //Obtem o token na url e chama a request
+  useEffect(() => {
+      const params = new URLSearchParams(window.location.hash.replace("#", "?"));
+      const accessToken = params.get("access_token");
+    if (accessToken) {
+      spotifyApi.setAccessToken(accessToken);
+      getTopTracks("long_term", limitTracks);
+    }
+  }, []);
+
+  const getTopTracksOfLimit = (newLimit: number) => {
+    setLimitTracks(newLimit)
+    getTopTracks(typeTime, newLimit)
+  }
+
+  const getTopTracksOfTimeRange = (newTimeRange: TimeRange) => {
+    setTypeTime(newTimeRange)
+    getTopTracks(newTimeRange, limitTracks)
+  }
+
+  return (
+    <>
+      <div className="xs:px-[400px] text-white h-full bg-black">
+          <h1 className="text-[20px] xs:text-4xl">Your Top Spotify tracks</h1>
+        <div className="w-full flex justify-center pt-5">
+        </div>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <>
+           <LimitSelector getTopTracksOfLimit={getTopTracksOfLimit} limitTracks={limitTracks}/>
+            <TimeRangeSelector getTopTracksOfTimeRange={getTopTracksOfTimeRange} typeTime={typeTime}/>
+            <PrintContainer domEl={domEl}>
+             <HeaderTracks text={showTypeTime(typeTime)} />
+              {tracks.map((tracks, index) => (
+                <>
+                  <TopFiveTracks
+                    id={tracks.id}
+                    artistName={tracks.artists[0].name}
+                    trackName={tracks.name}
+                    positionTrack={index}
+                    trackImage={tracks.album.images[0].url}
+                    key={tracks.id}
+                  />
+                </>
+              ))}
+              <div className="w-full flex justify-center text-2xl">
+              <SpotifyIcon width="35" height="35" fill="#fff" />
+              <span className="ml-2"> Spotify</span>
+              </div>
+            </PrintContainer>
+          </>
+        )}
+      </div>
+    </>
+  );
 }
+
+export default TopTracks;
