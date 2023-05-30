a9ea42cf084dc9a7343d3a16df3329e697cc4554
Yan <yangabriel2012@gmail.com>
2023-05-29 22:44:48 -0300

    feat: criando component LimitSelector

---
+++ src/components/LimitSelector.tsx
@@ -0,0 +1,24 @@
+interface LimitSelectorProps{
+    limitTracks: number,
+    getTopTracksOfLimit: (newLimit: number) => void
+}
+export const LimitSelector = ({getTopTracksOfLimit, limitTracks}: LimitSelectorProps) => {
+    return(
+        <div className="w-full flex justify-center pt-8">
+        <span
+          className={`px-4 ${limitTracks === 5 && "selectedTime"}`}
+          onClick={() => getTopTracksOfLimit(5)}
+        >
+          Top 5
+        </span>
+        <span
+          className={`px-4 ${
+            limitTracks === 10 &&  "selectedTime"
+          }`}
+          onClick={() => getTopTracksOfLimit(10)}
+        >
+          Top 10
+        </span>
+      </div>
+    )
+}
