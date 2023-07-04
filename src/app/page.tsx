99d119686284c39687bfbf79b33a5b58bd965fde
Yan <yangabriel2012@gmail.com>
2023-07-03 22:58:19 -0300

    fix: identando codigo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { AuthButton } from "@/components/AuthButton";
 
-
 export default function Home() {
+
   return (
     <div className="h-screen w-full bg-no-repeat bg-center bg-cover bg-[url('/images/photo.avif')]">
       <div className="absolute h-screen w-full backdrop-brightness-50">
@@ -21,7 +21,8 @@ export default function Home() {
         >
           <div className="text-white text-center mt-10 xs:text-sm md:text-base text-xs max-w-md">
             <h2>
-              Você é um amante da música e adora acompanhar as suas tracks favoritas no Spotify?
+              Você é um amante da música e adora acompanhar as suas tracks
+              favoritas no Spotify?
             </h2>
             <h3 className="text-white mt-6">
               Então não perca mais tempo! Faça login agora no nosso site e
@@ -29,10 +30,8 @@ export default function Home() {
             </h3>
           </div>
         </div>
-        <div
-          className="w-full mt-10 flex items-center justify-center animate-[wiggle_1s_ease]"
-        >
-          <AuthButton/>
+        <div className="w-full mt-10 flex items-center justify-center animate-[wiggle_1s_ease]">
+          <AuthButton />
         </div>
       </div>
     </div>
