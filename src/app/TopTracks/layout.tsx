1b4baf587601e22b3780f8731798a28988ca9758
Yan <yangabriel2012@gmail.com>
2023-07-04 22:39:26 -0300

    fix: removendo codigo desnecessario

--- src/app/TopTracks/layout.tsx
+++ src/app/TopTracks/layout.tsx
@@ -1,12 +1,5 @@
 import { Footer } from '@/components/Footer'
 import '../../styles/globals.css'
-import { Poppins } from 'next/font/google'
-
-const poppins = Poppins({
-    subsets: ['latin'],
-    weight: "400",
-    variable: '--font-poppins'
-})
 
 export default function RootLayout({
   children,
@@ -15,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} font-sans bg-black`}>
+      <body>
         {children}
       <Footer/>
       </body>
